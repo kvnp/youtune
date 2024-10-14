079d6d490e9870cbc4d94a88e23f209d9356a04c
Kevin Pfeifer <kevin.pfeifer@live.de>
2024-10-14 22:24:38 +0200

    add forgotten functions

--- public/image/worker.js
+++ public/image/worker.js
@@ -555,6 +555,15 @@ function post(rgb) {
     self.postMessage({rgb, imageHex, buttonHex, fontHex, thumbHex});
 }
 
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 self.onmessage = ({data: {pixels, width, height}}) => {
     ColorThief.getColorNoCanvas(new Uint8Array(pixels), width, height, 10, post);
 };
