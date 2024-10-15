beeb36c2fe6d052e515e9f7be2782355b264b0dc
Kevin Pfeifer <kevin.pfeifer@live.de>
2024-10-15 12:24:05 +0200

    remove unused babel plugins

--- babel.config.js
+++ babel.config.js
@@ -5,10 +5,6 @@ export default function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "@babel/plugin-transform-flow-strip-types",
-      ["@babel/plugin-proposal-private-methods", { loose: true }],
-      ["@babel/plugin-transform-class-properties", { loose: true }],
-      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
       ["transform-react-remove-prop-types", {
         removeImport: true,
         additionalLibraries: ["react-style-proptype"]
